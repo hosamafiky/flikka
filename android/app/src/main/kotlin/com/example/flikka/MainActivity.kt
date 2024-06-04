c67c88b5e4770d1f24196f743911f5bcdd0d6595
Hussam <hosamafiky@gmail.com>
2024-06-04 21:19:08 +0300

    chore: Add iOS and Android project files and configurations

---
+++ android/app/src/main/kotlin/com/example/flikka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flikka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/onboarding/presentation/cubit/onboarding_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'onboarding_state.dart';
+
+class OnboardingCubit extends Cubit<OnboardingState> {
+  OnboardingCubit() : super(OnboardingInitial());
+}
